b84abf912efe9d973186aae4192a0adaec058d0c
SunSung W541 <mail@embedded.biz.ua>
2024-05-23 02:00:24 +0200

    заготовка под модуль

---
+++ const.go
@@ -0,0 +1,5 @@
+package htmlValidator
+
+const GlobalVersion string = "0.0.0"
+const GlobalDateUpdate string = "05-23-2024"
+const GlobalName string = "htmlValidator"
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fRead-dev/htmlValidator
+
+go 1.21
