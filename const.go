ef6adb8e132704298aec2e9830a041ea311a6e08
SunSung W541 <mail@embedded.biz.ua>
2024-05-23 16:22:57 +0200

    * main [1.8.1] :  git

--- const.go
+++ const.go
@@ -1,5 +1,5 @@
 package htmlValidator
 
-const GlobalVersion string = "1.7.1"
+const GlobalVersion string = "1.7.2"
 const GlobalDateUpdate string = "05-23-2024"
 const GlobalName string = "htmlValidator"
