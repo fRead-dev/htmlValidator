16d95f11c34527cac125ca71a8c05d280687da01
SunSung W541 <mail@embedded.biz.ua>
2024-05-23 08:17:56 +0200

    * main [1.5.5] :  MD

--- const.go
+++ const.go
@@ -1,5 +1,5 @@
 package htmlValidator
 
-const GlobalVersion string = "1.5.3"
+const GlobalVersion string = "1.5.4"
 const GlobalDateUpdate string = "05-23-2024"
 const GlobalName string = "htmlValidator"
